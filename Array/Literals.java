92212724199e37be9396d8171cf1d4425238298e
kushalkumarreddy <kushalkumarreddykona@gmail.com>
2026-07-28 20:16:24 +0530

    Added programs

---
+++ Array/Literals.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class Literals {
+	public static void main(String args[]) {
+		String names[] = {"bahubali","devasena","ballala","kattapa"};
+		System.out.println(names[0]);
+		System.out.println(names[1]);
+		System.out.println(names[2]);
+		System.out.println(names[3]);
+		
+		for(int i=0;i<=names.length;i++) {
+			System.out.println(names[i]);
+			
+		}
+	}
+
+}
--- Methods/MethodDemo.java
+++ Methods/MethodDemo.java
@@ -1,7 +1,43 @@
 package Methods;
 
 public class MethodDemo {
+	public static void method1()
+	{
+		System.out.println("welcome");
+	}
+
+
+	public static int method2()
+	{
+		return 78;
+	}
+
+
+	public static void method3(int age)
+	{
+		System.out.println("my age is :"+age);
+	}
+
+
+	public static int method4(String s)
+	{
+		return s.length();
+	}
 	
+	public static double salary(int salary) {
+		return salary;
+	}
+	
+	public static void main(String args[]) {
+		MethodDemo md = new MethodDemo();
+		md.method1();
+		md.method2();
+		md.method3(78);
+		md.method4("pushpa");
+		System.out.println(md.salary(898));
+
+
+	}
 	
 
 }

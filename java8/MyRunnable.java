fd505727f13946b74d6431cbbd671f28c3db937d
kushalkumarreddy <kushalkumarreddykona@gmail.com>
2026-08-19 15:35:10 +0530

    Added program

---
+++ java8/MyRunnable.java
@@ -0,0 +1,33 @@
+package java8;
+
+public class MyRunnable {
+	//implements Runnable
+	
+//		@Override
+//		public void run() {
+//			for(int i=0;i<=10;i++) {
+//				System.out.println("Seetha thread");
+//			}
+//		}
+		
+		public static void main(String[] args) {
+			
+			//using lambda
+			Runnable r=() -> {
+				for(int i=0;i<=10;i++) {
+					System.out.println("Seetha thread");
+				}				
+			};
+			
+//			MyRunnable mr = new MyRunnable();
+			
+			Thread t = new Thread(r);
+			t.start();
+			
+			for(int i=0;i<=10;i++) {
+				System.out.println("Rama thread");
+			}
+			
+		}
+
+}

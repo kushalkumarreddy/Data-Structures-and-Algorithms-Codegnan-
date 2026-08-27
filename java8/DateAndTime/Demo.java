62c266e83904052602dd881bc8f278760fa300df
kushalkumarreddy <kushalkumarreddykona@gmail.com>
2026-08-27 09:36:16 +0530

    updated

--- java8/DateAndTime/Demo.java
+++ java8/DateAndTime/Demo.java
@@ -1,8 +1,13 @@
 package java8.DateAndTime;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Demo {
 	public static void main(String[] args) {
@@ -35,11 +40,40 @@ public class Demo {
 		System.out.println("nano: " + time.getNano());
 		
 		//set date time
-		System.out.println();
+		System.out.println();		
+		LocalDate dt = date.of(1994, 06, 05);
+		System.out.println(dt);
+		LocalTime st = time.of(12,12,12,12);
+		System.out.println(st);
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		System.out.println(formatter);
 		
-		LocalDate sd = date.of(1994, 06, 05);
-		DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-		System.out.println(format);
+		String result = dt.format(formatter);
+		System.out.println(result);
+		
+		//between
+		LocalDate bdy = LocalDate.of(1994,06,04);
+		LocalDate tdy = LocalDate.now();
+		
+		Period period = Period.between(bdy, tdy);
+		System.out.println(period.getYears()+" "+period.getMonths()+" "+period.getDays());
+		
+		//zone
+		ZonedDateTime india = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+		System.out.println(india);
+		
+		ZonedDateTime usa = ZonedDateTime.now(ZoneId.of("America/New_York"));
+		System.out.println(usa);
+		
+		LocalDateTime ldt = LocalDateTime.now();
+		System.out.println(ldt);
+		
+		
+//		//year
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter a Year Number: ");
+//		int n = sc.nextInt();
+		
 	}
 
 }

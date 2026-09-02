448bf2f5ff55bf14afcb06c2f9c31bff37318ee6
kushalkumarreddy <kushalkumarreddykona@gmail.com>
2026-09-02 14:34:28 +0530

    updated

--- Arrays/ArrayTraversal.java
+++ Arrays/ArrayTraversal.java
@@ -1,5 +1,13 @@
 package Arrays;
 
 public class ArrayTraversal {
+	public static void main(String[] args) {
+		int arr[] = {23,87,16,65,47};
+		
+		System.out.println("array is traversing");
+		for(int i=0;i<arr.length;i++) {
+			System.out.println("index "+i+" value "+arr[i]);
+		}
+	}
 
 }
